d2697d16786f0816744ae753c93f02ec0913219c
Toreakhmet228 <113284200+Toreakhmet228@users.noreply.github.com>
2024-12-23 04:59:13 +0500

    Update views.py

--- news/views.py
+++ news/views.py
@@ -1,8 +1,9 @@
+
 from rest_framework.views import APIView
 from rest_framework.response import Response
-from rest_framework.generics import ListAPIView
-from .models import News, Category
-from .serializers import NewsSerializer, CategorySerializer
+from rest_framework.generics import ListAPIView, CreateAPIView
+from .models import News, Category, Comment, Like
+from .serializers import NewsSerializer, CategorySerializer, CommentSerializer
 
 class NewsListView(ListAPIView):
     queryset = News.objects.all().order_by('-date_added')
@@ -16,3 +17,29 @@ class NewsByCategoryView(APIView):
         news = News.objects.filter(category=category).order_by('-date_added')
         serializer = NewsSerializer(news, many=True)
         return Response(serializer.data)
+
+class AddCommentView(CreateAPIView):
+    queryset = Comment.objects.all()
+    serializer_class = CommentSerializer
+
+    def perform_create(self, serializer):
+        news_id = self.request.data.get('news')
+        news = News.objects.get(id=news_id)
+        user_id = self.request.data.get('user')
+        user = User.objects.get(id=user_id)
+        serializer.save(user=user, news=news)
+
+class LikeNewsView(APIView):
+    def post(self, request, news_id):
+        news = News.objects.filter(id=news_id).first()
+        if not news:
+            return Response({"error": "News not found"}, status=404)
+
+        user_id = request.data.get('user')
+        user = User.objects.get(id=user_id)
+
+        like, created = Like.objects.get_or_create(user=user, news=news)
+        if not created:
+            like.delete()
+            return Response({"message": "Like removed"})
+        return Response({"message": "News liked"})
