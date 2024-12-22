72aad6be1233aab9c4a39c57cedd180f8b04f32a
Toreakhmet228 <113284200+Toreakhmet228@users.noreply.github.com>
2024-12-23 04:58:54 +0500

    Update urls.py

--- news_site/urls.py
+++ news_site/urls.py
@@ -10,6 +10,10 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('news/', NewsListView.as_view(), name='news-list'),
     path('news/<str:category_name>/', NewsByCategoryView.as_view(), name='news-by-category'),
+    path('news/comment/add/', AddCommentView.as_view(), name='add_comment'),
+        path('news/<int:news_id>/like/', LikeNewsView.as_view(), name='like_news'),
+
+
 ]
 if settings.DEBUG:
     urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
